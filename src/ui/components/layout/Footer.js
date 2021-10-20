1e25da2f1a8f1bb39d8b37b78697a1d980f9930b
chekin <santiisalcedo129@gmail.com>
2021-10-19 21:48:16 -0300

    footer styles

--- src/ui/components/layout/Footer.js
+++ src/ui/components/layout/Footer.js
@@ -3,10 +3,10 @@ import Nav from "./Nav";
 const Footer = () => {
 
     return (
-        <footer id="">
+        <footer id="footer">
             <div className="footer-content">
                 <p>
-                    lorem ipsum
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 </p>
             </div>
             <div className="footer-nav">
